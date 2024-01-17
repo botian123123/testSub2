8ef508e7bf9798aa59520eeda8233d0bb4eb1f47
botian123123 <bo.tian@pax.us>
2024-01-17 14:39:28 -0500

    init

---
+++ sub2/src/main/java/com/example/sub2/Modle2.java
@@ -0,0 +1,15 @@
+package main.java.com.example.sub2;
+
+
+public class Modle2 {
+    String name = "civic";
+    String type = "seden";
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
